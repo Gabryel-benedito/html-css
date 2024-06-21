45ad11fd344ce239d87fc1a1061f3c1ebd14e58e
Gabryel Benedito Monteiro <gabryel3701@gmail.com>
2024-06-21 15:04:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
-    //ficar com a cor fixa quando clica
+//ficar com a cor fixa quando clica
 const menuItem = window.document.querySelectorAll('.menu-item');
 
 function selectLink() {
-    menuItem.forEach((item)=>
+    menuItem.forEach((item) =>
         item.classList.remove('ativo')
     )
     this.classList.add('ativo')
 };
 
-menuItem.forEach((item)=>
-    item.addEventListener('click' , selectLink)
+menuItem.forEach((item) =>
+    item.addEventListener('click', selectLink)
 );
 
 //expandir o menu
@@ -19,7 +19,7 @@ const btnesp = window.document.querySelector('#btn-esp');
 const menu = window.document.querySelector('.menu-conteiner');
 
 
-btnesp.addEventListener('click' , ()=> {
+btnesp.addEventListener('click', () => {
     menu.classList.toggle('btn-expandir');
 });
 
@@ -30,17 +30,17 @@ btnesp.addEventListener('click' , ()=> {
 const main = window.document.getElementById('main-conteiner');
 const textoEscondido = window.document.querySelectorAll('.text');
 
-    function aparecerTexto(evento) {
-        textoEscondido.forEach((texto) => {
-            texto.classList.remove('text-ativo')
-        });
-        
-       evento.currentTarget.classList.add('text-ativo')
+function aparecerTexto(evento) {
+    textoEscondido.forEach((texto) => {
+        texto.classList.remove('text-ativo')
+    });
 
-    };
+    evento.currentTarget.classList.add('text-ativo')
+
+};
 
 
-main.addEventListener('click' , () => {
+main.addEventListener('click', () => {
     main.classList.toggle('main-ativo');
     textoEscondido.forEach((texto) => {
         texto.classList.toggle('text-ativo')
