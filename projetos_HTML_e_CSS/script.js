173bf2f9f0197feb2716657088be0a1537712f66
Gabryel Benedito Monteiro <gabryel3701@gmail.com>
2024-06-29 19:34:35 -0300

    update no projeto

--- projetos_HTML_e_CSS/script.js
+++ projetos_HTML_e_CSS/script.js
@@ -5,3 +5,18 @@ const butão =  window.document.getElementById('btn-menu');
 butão.addEventListener('click' , () => {
     menuConteiner.classList.toggle('btn-expandir')
 });
+
+
+const foto = window.document.getElementById('foto');
+
+foto.addEventListener('mouseenter' , () => {
+    foto.classList.add('foto-ativa');
+    foto.classList.add('cont-ativo');
+})
+
+foto.addEventListener('mouseout' , () => {
+    foto.classList.remove('foto-ativa');
+    foto.classList.remove('cont-ativo');
+})
+
+
